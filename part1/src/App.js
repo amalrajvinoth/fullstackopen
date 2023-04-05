75fd39891da2f8c41e7680018b248b1b0239fac4
Amal <amalrajvinoth@gmail.com>
2023-04-05 23:03:44 +0200

    1.7: unicafe step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,33 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-const Display = ({ text, counter }) => <div>{text} {counter}</div>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Display = ({text, counter}) => <div>{text} {counter}</div>
 
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+    const increaseGood = () => setGood(good + 1)
+    const increaseNeutral = () => setNeutral(neutral + 1)
+    const increaseBad = () => setBad(bad + 1)
 
-  return (
-      <div>
-        <div> <h1>give feedback</h1> </div>
-        <Button handleClick={increaseGood} text="good" />
-        <Button handleClick={increaseNeutral} text="netural" />
-        <Button handleClick={increaseBad} text="bad" />
-        <div> <h1>statistics</h1> </div>
-        <Display text="good" counter={good} />
-        <Display text="neutral" counter={neutral} />
-        <Display text="bad" counter={bad} />
-      </div>
-  )
+    return (
+        <div>
+            <div><h1>give feedback</h1></div>
+            <Button handleClick={increaseGood} text="good"/>
+            <Button handleClick={increaseNeutral} text="netural"/>
+            <Button handleClick={increaseBad} text="bad"/>
+            <div><h1>statistics</h1></div>
+            <Display text="good" counter={good}/>
+            <Display text="neutral" counter={neutral}/>
+            <Display text="bad" counter={bad}/>
+            <Display text="all" counter={good + neutral + bad}/>
+            <Display text="average" counter={(good + neutral + bad) / 3}/>
+            <Display text="positive" counter={(100 * good) / (good + neutral + bad) + "%"}/>
+        </div>
+    )
 }
 
 export default App
